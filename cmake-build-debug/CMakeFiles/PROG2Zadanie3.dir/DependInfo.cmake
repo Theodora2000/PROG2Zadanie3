7b652a0412aa9c77f34a0c639efb51a67f9708b1
Theodora2000 <xsimont@stuba.sk>
2021-03-31 18:16:03 +0200

    argumenty

---
+++ cmake-build-debug/CMakeFiles/PROG2Zadanie3.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/WIN10/Desktop/FEI/4.semeter/PROG2/Zadania/PROG2Zadanie3/main.c" "C:/Users/WIN10/Desktop/FEI/4.semeter/PROG2/Zadania/PROG2Zadanie3/cmake-build-debug/CMakeFiles/PROG2Zadanie3.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/PROG2Zadanie3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PROG2Zadanie3.dir/main.c.obj"
+  "PROG2Zadanie3.exe"
+  "PROG2Zadanie3.exe.manifest"
+  "PROG2Zadanie3.pdb"
+  "libPROG2Zadanie3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PROG2Zadanie3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
