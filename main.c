d7693eefa13e681311a4be2df1a50d8f882ef873
Theodora2000 <xsimont@stuba.sk>
2021-04-01 12:58:42 +0200

    return 1,2,3

--- main.c
+++ main.c
@@ -1,33 +1,81 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+
+char *readline() {
+    const size_t BSIZE = 100;
+    char *line = NULL;
+    size_t capacity = 0;
+
+    do {
+        // Priprav dostatok pamate.
+        capacity += BSIZE;
+
+        // Alokuj miesto na capacity + '\0'.
+        line = (char *) realloc(line, (capacity + 1) * sizeof(char));
+        if (line == NULL)
+            return line;
+
+        // Nacitaj zo vstupu riadok (resp. jeho zvysok).
+        if (fgets(line + capacity - BSIZE, BSIZE + 1, stdin) == NULL) {
+            free(line);
+            return NULL; // doslo k chybe citania alebo sme narazili na EOF
+        }
+        // Opakuj, pokym sa nenacita cely riadok.
+        // Indikacia, ze zostal zvysok riadku:
+        //   1. je naplnena cela BSIZE
+        //   2. na konci nie je '\n'
+    } while (strlen(line + capacity - BSIZE) >= BSIZE
+             && line[capacity - 1] != '\n');
+    // nacitany riadok obsahuje na konci znak '\n'
+    return line;
+}
 
 
 int main(int argc, char * argv[]) {
-/*
-    for(int i=0;i<argc;i++){
-        printf("%i: %s\n",i,argv[i]);
-    }*/
-    int c;
-    int opa=0,opb=0,opc=0;
-    char* cval=0;
-    while((c = getopt(argc,argv,"abc:"))!=-1){
-        switch (c) {
+
+    int opt;
+    char* optstring = "aculr:";
+    char *rvalue = NULL;
+
+
+    while ((opt = getopt(argc, argv, optstring)) != -1) {
+        switch (opt) {
             case 'a':
-                opa=1;
+                printf("Prepinac -a\n");
                 break;
-            case 'b':
-                opb=2;
+            case 'r':
+                rvalue=optarg;
+                printf("Prepinac -r a jeho povinny parameter %s\n",rvalue);
                 break;
-            case'c':
-                opc=2;
-                cval=optarg;
+            case 'c':
+                printf("Prepinac -c\n");
+                break;
+            case 'u':
+                printf("Prepinac -u\n");
+                break;
+            case 'l':
+                printf("Prepinac -l\n");
                 break;
 
+            case '?':
+                if(optopt=='r'){
+                    printf("Prepinac -r vyzaduje povinny parameter\n");
+                    return 2;
+                }else if(isprint(optopt)){
+                    printf("Neplatny prepinac -%c", optopt);
+                    return 1;
+                }
 
         }
-
-
     }
-    printf("%i %i %i %s\n",opa,opb,opc,cval);
+    if(optind < argc){
+        for(int i=optind;i<argc;i++){
+            printf("Non-option CMD argument: %s\n",argv[i]);
+        }
+    }
+
+
     return 0;
 }
